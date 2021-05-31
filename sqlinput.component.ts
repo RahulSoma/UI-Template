23292133c78b84642878e10daf795ed3e5b18d1d
RahulSoma <rahulsoma85@gmail.com>
2021-05-31 17:37:24 +0530

    Add files via upload

---
+++ sqlinput.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sqlinput',
+  templateUrl: './sqlinput.component.html',
+  styleUrls: ['./sqlinput.component.scss']
+})
+export class SqlinputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ sqloutput.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sqloutput',
+  templateUrl: './sqloutput.component.html',
+  styleUrls: ['./sqloutput.component.scss']
+})
+export class SqloutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
